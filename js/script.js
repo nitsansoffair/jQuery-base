7ea267efbd8badc16afd12faabf3298e8eed7210
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-14 21:49:58 +0300

    Creating an Image Gallery with Lightbox Preview

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 $(function() {
-    $('#btn-click').click({
-        user: 'Peter',
-        domain: 'domain.com'
-    }, function (event) {
-        greetUser(event.data);
+    const galleryItems = $('.gallery').find('img');
+    galleryItems.css('width', '33%').css('opacity', '0.7');
+
+    galleryItems.mouseenter(function () {
+        $(this).stop().fadeTo(500, 1);
     });
 
-    function greetUser(userData) {
-        username = userData.user || 'Anonymous';
-        domain = userData.domain || 'Domain';
-        alert(username + ' ' + domain);
-    }
+    galleryItems.mouseleave(function () {
+        $(this).stop().fadeTo(500, 0.7);
+    });
+
+    galleryItems.click(function () {
+        const source = $(this).attr('src');
+        const image = $('<img>').attr('src', source).css('width', '100%');
+        $('.lightbox').empty().append(image).fadeIn(2000);
+
+        $('.lightbox').click(function () {
+            $(this).stop().fadeOut();
+        });
+    });
 });
