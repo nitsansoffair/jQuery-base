bcaa79de6d7655da8dd4c7107c1bfd8778b64ab1
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-14 18:28:04 +0300

    Changing the CSS Properties of Elements

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 $(function() {
-    const galleryImage = $('.gallery').find('img').first();
-    const images = [
-        './images/laptop-mobile_small.jpg',
-        './images/laptop-on-table_small.jpg',
-        './images/people-office-group-team_small.jpg'
-    ];
+    $('.gallery').css('display', 'none'); // .hide();
 
-    let i = 0;
-    setInterval(function () {
-        i = (i + 1) % images.length;
-        galleryImage.fadeOut(function () {
-            $(this).attr('src', images[i]);
-            $(this).fadeIn();
-        });
-        console.log(galleryImage.attr('src'));
-    }, 2000);
+    const redBox = $('.red-box');
+    console.log(redBox.css('width'));
+    console.log(redBox.width());
+
+    redBox.css('background-color', '#AA7700');
+    $('p').css('font-size', '18px');
+    redBox.css('width', '+=20px');
+
+    const properties = $('p').css(['font-size', 'line-height', 'color']);
+    console.log(properties);
+
+    redBox.css('user-select', 'none');
 });
