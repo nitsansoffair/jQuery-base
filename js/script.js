7f28795846ab33516577ab6d906142aa626953f7
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-14 19:24:29 +0300

    Retrieving and Changing the Content of an Element

--- js/script.js
+++ js/script.js
@@ -1,20 +1,11 @@
 $(function() {
-    const gallery = $('.gallery');
-    const images = [
-        './images/laptop-mobile_small.jpg',
-        './images/laptop-on-table_small.jpg',
-        './images/people-office-group-team_small.jpg'
-    ];
-
-    gallery.data('availableImages', images);
-    console.log(gallery.data('availableImages'));
-
-    gallery.data('name', 'The Awesome Gallery');
-    console.log(gallery.data());
-
-    gallery.removeData('name');
-    console.log(gallery.data());
-
     const firstPar = $('p:first');
-    console.log(firstPar.data('mydata'));
+    console.log(firstPar.text());
+    console.log(firstPar.html());
+
+    console.log($('p').html());
+    firstPar.text('<strong>Hello</strong>');
+
+    firstPar.html('<strong>Hello</strong> World!');
+    firstPar.html(firstPar.html() + ' appended.');
 });
