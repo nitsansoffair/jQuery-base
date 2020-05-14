23eb6f1fa83d5a02cc6bc6c64c3e3e61c50e5946
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-14 17:28:49 +0300

    Adding New Elements to the DOM

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 $(function() {
-    // $('#list').find('li').filter(':even').css('background-color', 'rgba(180, 180, 30, 0.8');
-    // $('li').eq(-1).css('background-color', 'rgba(180, 180, 30, 0.8');
-    $('li').not(':first').css('background-color', 'rgba(180, 180, 30, 0.8');
+    // $('ul ul:first').append('<li>last</li>');
+    // $('<li>last</li>').appendTo($('ul ul:first'));
+
+    // $('ul ul:first').prepend('<li>prepend</li>');
+    // $('<li>prepend</li>').prependTo($('ul ul:first'));
+    // $('<h4>prepend</h4>').prependTo('#content');
+
+    // $('.red-box').after('<div class="red-box">Red After</div>');
+    // $('.blue-box').before('<div class="blue-box">Blue Before</div>');
+
+    // $('.blue-box').before(function () {
+    //     return '<div class="blue-box">Blue 2</div>';
+    // });
+
+    $('p').after($('#list'));
 });
