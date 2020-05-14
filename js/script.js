405ef497872d1a42ade7625c9d23ba590a215202
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-14 12:33:03 +0300

    Sliding Elements Up and Down

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 $(function() {
-  // jQuery goes here...
-
-  // Uncomment this line to fade out the red box on page load
-  $('.red-box').fadeTo(3000, 0.2);
-  $('.green-box').fadeTo(2000, 0.5);
-  $('.yellow-box').fadeTo(1000, 0.8);
-
-  $('.green-box').fadeToggle();
-  $('.green-box').fadeToggle();
+  $('.blue-box').slideToggle(2000);
+  $('p').hide();
+  $('p').slideDown(1000);
 });
