ed54311da260e905242ecb341a0ed1b2c2a3e678
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-14 12:57:52 +0300

    Timing Animations Using Callback Functions

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
 $(function() {
-    $('.red-box').fadeTo(1000, 0.2);
-
-    $('.green-box')
-        .delay(1000)
-        .fadeTo(1000, 0.5);
-
-    $('.blue-box')
-        .delay(2000)
-        .fadeTo(1000, 0.8)
-        .fadeOut()
-        .delay(500)
-        .fadeIn();
+    $('.red-box').fadeTo(1000, 0, function () {
+        $('.green-box').fadeTo(1000, 0, function () {
+            $('.blue-box').fadeTo(1000, 0);
+        });
+    });
 });
