53e582c09468d3481850bfa3483d5c644a072cae
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-14 17:12:45 +0300

    jQuery Methods for Filtering

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 $(function() {
-    // $('#list').find('li').css('background-color', 'rgba(180, 180, 30, 0.8)');
-    // $('#list').children('li').css('background-color', 'rgba(180, 180, 30, 0.8)');
-    // $('#list').parents('div').css('background-color', 'rgba(180, 180, 30, 0.8)');
-    // $('#list').siblings().css('background-color', 'rgba(180, 180, 30, 0.8)');
-    // $('#list').siblings(':header').css('background-color', 'rgba(180, 180, 30, 0.8)');
-    // $('#list').prev().css('background-color', 'rgba(180, 180, 30, 0.8)');
-    $('#list').next().css('background-color', 'rgba(180, 180, 30, 0.8)');
+    // $('#list').find('li').filter(':even').css('background-color', 'rgba(180, 180, 30, 0.8');
+    // $('li').eq(-1).css('background-color', 'rgba(180, 180, 30, 0.8');
+    $('li').not(':first').css('background-color', 'rgba(180, 180, 30, 0.8');
 });
