f950c5a9accab82bc2f6b603fd015d2605d60919
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-14 21:57:22 +0300

    Handling KeyDown and KeyUp Events

--- js/script.js
+++ js/script.js
@@ -1,22 +1,9 @@
 $(function() {
-    const galleryItems = $('.gallery').find('img');
-    galleryItems.css('width', '33%').css('opacity', '0.7');
-
-    galleryItems.mouseenter(function () {
-        $(this).stop().fadeTo(500, 1);
-    });
-
-    galleryItems.mouseleave(function () {
-        $(this).stop().fadeTo(500, 0.7);
-    });
-
-    galleryItems.click(function () {
-        const source = $(this).attr('src');
-        const image = $('<img>').attr('src', source).css('width', '100%');
-        $('.lightbox').empty().append(image).fadeIn(2000);
-
-        $('.lightbox').click(function () {
-            $(this).stop().fadeOut();
-        });
+    $('html').keydown(function (event) {
+        if (event.which === 39) {
+            $('.blue-box').stop().animate({
+                'margin-left': '+=10px'
+            }, 50);
+        }
     });
 });
