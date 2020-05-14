93427907dda940fe7427e502c30525b85d2ce0f0
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-14 13:22:08 +0300

    jQuery Methods for Traversal

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 $(function() {
-    // $('p').css('background-color', 'rgba(180, 180, 30, 0.8)');
-    // $('.red-box').css('background-color', 'rgba(180, 180, 30, 0.8)');
-    // $('#list').css('background-color', 'rgba(180, 180, 30, 0.8)');
-    // $('input[type="text"]').css('background-color', 'rgba(180, 180, 30, 0.8)');
-    // $('h2, p, input').css('background-color', 'rgba(180, 180, 30, 0.8)');
-    // $('p:first').css('background-color', 'rgba(180, 180, 30, 0.8)');
-    // $('li:last').css('background-color', 'rgba(180, 180, 30, 0.8)');
-    // $('li:odd').css('background-color', 'rgba(180, 180, 30, 0.8)');
-    // $('li:odd').css('background-color', 'rgba(180, 180, 30, 0.8)');
-
-    $('input:text').css('background-color', 'rgba(180, 180, 30, 0.8');
+    // $('#list').find('li').css('background-color', 'rgba(180, 180, 30, 0.8)');
+    // $('#list').children('li').css('background-color', 'rgba(180, 180, 30, 0.8)');
+    // $('#list').parents('div').css('background-color', 'rgba(180, 180, 30, 0.8)');
+    // $('#list').siblings().css('background-color', 'rgba(180, 180, 30, 0.8)');
+    // $('#list').siblings(':header').css('background-color', 'rgba(180, 180, 30, 0.8)');
+    // $('#list').prev().css('background-color', 'rgba(180, 180, 30, 0.8)');
+    $('#list').next().css('background-color', 'rgba(180, 180, 30, 0.8)');
 });
