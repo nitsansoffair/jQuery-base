5ac7795a168c3489838e29172a504026b94e78d8
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-14 20:35:40 +0300

    Adding MouseEnter & MouseLeave Handlers

--- js/script.js
+++ js/script.js
@@ -6,4 +6,18 @@ $(function() {
     $('.green-box').hover(function () {
         $(this).text('Hover');
     });
+
+    const blueBox = $('.blue-box');
+    // blueBox.mouseenter(function () {
+    //     $(this).stop().fadeTo(500, 0.7);
+    // });
+    // blueBox.mouseleave(function () {
+    //     $(this).stop().fadeTo(500, 1);
+    // });
+
+    blueBox.hover(function () {
+        $(this).stop().fadeTo(500, 0.7);
+    }, function () {
+        $(this).stop().fadeTo(500, 1);
+    });
 });
