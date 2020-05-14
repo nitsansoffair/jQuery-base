721827e1498a347d503c5a07b5bf00d882ece313
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-14 18:14:32 +0300

    Creating an Image Slide show

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 $(function() {
-    // const specialLink = $('#special-link');
-    // console.log(specialLink.attr('href'));
+    const galleryImage = $('.gallery').find('img').first();
+    const images = [
+        './images/laptop-mobile_small.jpg',
+        './images/laptop-on-table_small.jpg',
+        './images/people-office-group-team_small.jpg'
+    ];
 
-    // specialLink.attr('href', 'http://change');
-
-    // const checkbox = $('input:checkbox');
-    // console.log(checkbox.prop('checked'));
-    // checkbox.prop('checked', false);
-
-    const textInput = $('input:text');
-    textInput.val('Change');
-    console.log(textInput.val());
-
-    const rangeInput = $('input[type="range"]');
-    console.log(rangeInput.val());
+    let i = 0;
+    setInterval(function () {
+        i = (i + 1) % images.length;
+        galleryImage.fadeOut(function () {
+            $(this).attr('src', images[i]);
+            $(this).fadeIn();
+        });
+        console.log(galleryImage.attr('src'));
+    }, 2000);
 });
