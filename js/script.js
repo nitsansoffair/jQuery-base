a725cde487022aa131e51d6590411e7a983ae47b
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-15 15:08:05 +0300

    Complete Form Validation on Submit

--- js/script.js
+++ js/script.js
@@ -1,10 +1,61 @@
 $(function() {
     $('#form').submit(function (event) {
-        const textarea = $('#message');
+        const name = $('#name').val();
+        const password = $('#password').val();
+        const message = $('#message').val();
+        const checked = $('#checkbox').is(':checked');
 
-        if (textarea.val().trim() === '') {
-            textarea.css('box-shadow', '0 0 4px #881');
-            event.preventDefault();
-        } else {}
+        validateNameField(name, event);
+        validatePasswordField(password, event);
+        validateMessageField(message, event);
+        validateCheckboxField(checked, event);
     });
 });
+
+function validateNameField(name, event) {
+    if (!isValidName(name)) {
+        $('#name-feedback').text('Please enter at least two characters.');
+        event.preventDefault();
+    } else {
+        $('#name-feedback').text('');
+    }
+}
+
+function isValidName(name) {
+    return name.length >= 2;
+}
+
+function validatePasswordField(password, event) {
+    if (!isValidPassword(password)) {
+        $('#password-feedback').text('Please enter at least 6 characters and contain a number.');
+        event.preventDefault();
+    } else {
+        $('#password-feedback').text('');
+    }
+}
+
+function isValidPassword(password) {
+    return password.length >= 6 && /.*[0-9].*/.test(password);
+}
+
+function validateMessageField(message, event) {
+    if (!isValidMessage(message)) {
+        $('#message-feedback').text('Please enter a message.');
+        event.preventDefault();
+    } else {
+        $('#message-feedback').text('');
+    }
+}
+
+function isValidMessage(message) {
+    return message.trim() !== '';
+}
+
+function validateCheckboxField(isChecked, event) {
+    if (!isChecked) {
+        $('#checkbox-feedback').text('Please agree to this.');
+        event.preventDefault();
+    } else {
+        $('#checkbox-feedback').text('');
+    }
+}
