0a4f5ab3f5ab1cd02e3af394270059115051cf82
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-15 14:39:52 +0300

    Using the Change Event

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
 $(function() {
-    const inputFields = $('input:text, input:password, textarea');
-    inputFields.focus(function () {
-        $(this).css('box-shadow', '0 0 4px #666');
-    });
-
-    inputFields.blur(function () {
-        $(this).css('box-shadow', 'none');
-    });
-
-    $('#name').blur(function () {
-        const text = $(this).val();
-
-        if (text.length < 3) {
-            $(this).css('box-shadow', '0 0 4px #811');
+    $('#checkbox').change(function () {
+        const isChecked = $(this).is(':checked');
+        if (isChecked) {
+            $(this).add('label[for="cb"]').css('box-shadow', '0 0 4px #181');
         } else {
-            $(this).css('box-shadow', '0 0 4px #666');
+            $(this).add('label[for="cb"]').css('box-shadow', '0 0 4px #811');
         }
+    });
+
+    $('#selection').change(function () {
+        const selectedOption = $(this).find(':selected').text();
+        alert(selectedOption);
     });
 });
