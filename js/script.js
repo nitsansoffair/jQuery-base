55c19f2d32c7ac362a32547bae6f6974fdcbdd97
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-15 18:45:46 +0300

    Improving the Pokédex

--- js/script.js
+++ js/script.js
@@ -1,11 +1,37 @@
 $(function () {
     const pokeapiUrl = 'https://pokeapi.co/api/v2/generation/1';
+    const pokemonByName = 'https://pokeapi.co/api/v2/pokemon/';
 
     $.getJSON(pokeapiUrl)
         .done(function (data) {
             $.each(data.pokemon_species, function (index, pokemon) {
                 const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-                const par = $('<p>').html("Pokemon " + (index + 1) + " is " + name);
+                const link = $('<a>')
+                    .attr('id', pokemon.name)
+                    .attr('href', '#')
+                    .append(
+                        $('<strong>').text(name)
+                    );
+
+                const par = $('<p>').html("Pokemon " + (index + 1) + " is ").append(link);
+
+                link.click(function (event) {
+                    $.getJSON(pokemonByName + pokemon.name)
+                        .done(function (details) {
+                            const pokemonDiv = $('#pokemon-details');
+
+                            pokemonDiv.empty();
+                            pokemonDiv.append('<h2>' + name + '</h2>');
+                            pokemonDiv.append('<img src="' + details.sprites.front_default + '">');
+                            pokemonDiv.append('<img src="' + details.sprites.back_default + '">');
+                            pokemonDiv.append('<img src="' + details.sprites.front_shiny + '">');
+                            pokemonDiv.append('<img src="' + details.sprites.back_shiny + '">');
+                        })
+                        .fail(function () {});
+
+                    event.preventDefault();
+                });
+
                 par.appendTo('#pokemon');
             });
         })
