508b87202d77e2e0c45f5356cbb5dc2fdc6f5790
Nitsan Soffair <soffair@post.bgu.ac.il>
2020-05-15 15:25:04 +0300

    Fast Feedback Form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(function() {
+    const form = $('#form');
+    enableFastFeedback(form);
+
     $('#form').submit(function (event) {
         const name = $('#name').val();
         const password = $('#password').val();
@@ -11,6 +14,81 @@ $(function() {
         validateCheckboxField(checked, event);
     });
 });
+
+function enableFastFeedback(formElement) {
+    const nameInput = formElement.find('#name');
+    const passwordInput = formElement.find('#password');
+    const messageInput = formElement.find('#message');
+    const checkboxInput = formElement.find('#checkbox');
+
+    nameInput.blur(function (event) {
+        const name = $(this).val();
+        validateNameField(name, event);
+
+        if (!isValidName(name)) {
+            $(this).css({
+                'box-shadow': '0 0 4px #811',
+                'border': '1px solid #600'
+            });
+        } else {
+            $(this).css({
+                'box-shadow': '0 0 4px #181',
+                'border': '1px solid #060'
+            });
+        }
+    });
+
+    passwordInput.blur(function (event) {
+        const password = $(this).val();
+        validatePasswordField(password, event);
+
+        if (!isValidPassword(password)) {
+            $(this).css({
+                'box-shadow': '0 0 4px #811',
+                'border': '1px solid #600'
+            });
+        } else {
+            $(this).css({
+                'box-shadow': '0 0 4px #181',
+                'border': '1px solid #060'
+            });
+        }
+    });
+
+    messageInput.blur(function (event) {
+        const message = $(this).val();
+        validateMessageField(message, event);
+
+        if (!isValidMessage(message)) {
+            $(this).css({
+                'box-shadow': '0 0 4px #811',
+                'border': '1px solid #600'
+            });
+        } else {
+            $(this).css({
+                'box-shadow': '0 0 4px #181',
+                'border': '1px solid #060'
+            });
+        }
+    });
+
+    checkboxInput.change(function (event) {
+        const isChecked = $(this).is(':checked');
+        validateCheckboxField(isChecked, event);
+
+        if (!isChecked) {
+            $(this).add('label[for="cb"]').css({
+                'box-shadow': '0 0 4px #811',
+                'border': '1px solid #600'
+            });
+        } else {
+            $(this).add('label[for="cb"]').css({
+                'box-shadow': '0 0 4px #181',
+                'border': '1px solid #060'
+            });
+        }
+    });
+}
 
 function validateNameField(name, event) {
     if (!isValidName(name)) {
